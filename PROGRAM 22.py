69d818309fbf754ca222706f041599356fed952a
Sree Sharvesh P <ssreesharvesh@gmail.com>
2025-10-03 09:26:41 +0530

    Create PROGRAM 22.py

---
+++ PROGRAM 22.py
@@ -0,0 +1,22 @@
+def best_fit(blocks, processes):
+    allocation = [-1] * len(processes)
+    for i in range(len(processes)):
+        best_index = -1
+        for j in range(len(blocks)):
+            if blocks[j] >= processes[i]:
+                if best_index == -1 or blocks[j] < blocks[best_index]:
+                    best_index = j
+        if best_index != -1:
+            allocation[i] = best_index
+            blocks[best_index] -= processes[i]
+
+    print("\nProcess No.\tProcess Size\tBlock No.")
+    for i in range(len(processes)):
+        if allocation[i] != -1:
+            print(i+1, "\t\t", processes[i], "\t\t", allocation[i]+1)
+        else:
+            print(i+1, "\t\t", processes[i], "\t\tNot Allocated")
+
+blocks = list(map(int, input("Enter block sizes (space separated): ").split()))
+processes = list(map(int, input("Enter process sizes (space separated): ").split()))
+best_fit(blocks, processes)
