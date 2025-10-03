10000cfeb005a24ea07d4ef74365d2f802b69f40
Sree Sharvesh P <ssreesharvesh@gmail.com>
2025-10-03 09:32:12 +0530

    Create PROGRAM 35.py

---
+++ PROGRAM 35.py
@@ -0,0 +1,21 @@
+files = {}
+
+while True:
+    ch = input("\n1.Create File  2.Add Block  3.Display File  4.Exit: ")
+    if ch == "1":
+        fname = input("Enter file name: ")
+        files[fname] = []
+        print("File created.")
+    elif ch == "2":
+        fname = input("Enter file name: ")
+        if fname in files:
+            block = input("Enter block data: ")
+            files[fname].append(block)
+            print("Block added.")
+        else:
+            print("File not found.")
+    elif ch == "3":
+        for fname, blocks in files.items():
+            print(fname, "-> Index Block ->", blocks)
+    else:
+        break
